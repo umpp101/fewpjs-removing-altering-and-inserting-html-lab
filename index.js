49827df872e77902b5e98696748cae148505223a
Mustafa Muse <umpp101@icloud.com>
2019-12-09 13:49:59 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let element = document.getElementById("main")
+
+element.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Muse is the champion!"
+
+
+
+
